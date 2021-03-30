42c66a678b2db0e7f0bf460250043c57b6048475
Bruna Martins <bruna_martins0@hotmail.com>
2021-03-29 21:57:14 -0300

    Chess

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,20 +1,12 @@
 package boardgame;
 
 public class Position {
-	
-	private int column;
+
 	private int row;
+	private int column;
 	
 	public Position(int row, int column) {
-		this.column = column;
 		this.row = row;
-	}
-
-	public int getColumn() {
-		return column;
-	}
-
-	public void setColumn(int column) {
 		this.column = column;
 	}
 
@@ -25,11 +17,22 @@ public class Position {
 	public void setRow(int row) {
 		this.row = row;
 	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 	
 	@Override
 	public String toString() {
 		return row + ", " + column;
 	}
-	
-
 }
